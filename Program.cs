c16858701062605fea2664c47ff347405668d73e
BrianVBaker <brianbaker27@sky.com>
2018-06-11 11:41:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using log4net;
+using log4net;
 using System;
 using System.IO;
 using TestFramework.TestFramework;
@@ -12,7 +12,7 @@ namespace BatchRunner
     {
         static void Main(string[] args)
         {
-            Config.CSVfile = args[0];
+            Config.DIR = args[0];
             Config.Browser = args[1];
             Config.Delim = args[2];
             Config.Time = Convert.ToInt32(args[3]);
@@ -26,41 +26,53 @@ namespace BatchRunner
             ILog log = LogManager.GetLogger(typeof(Program));
 
             log.Info("Initialising........");
-
-            //RunTesting Testlines = new RunTesting();
-
-            string[] TestSteps;
-
-            using (StreamReader sr = new StreamReader(args[0]))
+            var csvFiles = Directory.EnumerateFiles(Config.DIR, "*.csv", SearchOption.AllDirectories);
+            int filect = 0;
+            foreach (string CSV in csvFiles)
             {
-                string TestFile = sr.ReadToEnd();
-                TestSteps = TestFile.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                filect++;
+                log.Info(filect + ". " + CSV);
             }
-
             
-
-            if (Config.Browser != "None")
-                TestRun.Driver = SeleniumActions.GetDriver(Config.Browser);
-
-            RunTesting.Steps(TestSteps, TestRun.Driver);
-
-            log.Info("Testing Summary");
-            log.Info("===============");
-            int passct = 0;
-            int failct = 0;
-            int totalct = 0;
-            foreach (var item in RunTesting.Results.Keys)
+            foreach (string currentFile in csvFiles)
             {
-                totalct++;
-                if (RunTesting.Results[item] == true)
-                    passct++;
-                else
-                    failct++;
+
+                //RunTesting Testlines = new RunTesting();
+                log.Info("Processing file : " + currentFile);
+
+                string[] TestSteps;
+
+                using (StreamReader sr = new StreamReader(currentFile))
+                {
+                    string TestFile = sr.ReadToEnd();
+                    TestSteps = TestFile.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                }
+
+
+
+                if (Config.Browser != "None")
+                    TestRun.Driver = SeleniumActions.GetDriver(Config.Browser);
+
+                RunTesting.Steps(TestSteps, TestRun.Driver);
+
+                log.Info("Testing Summary for file " + currentFile);
+                log.Info("===============");
+                int passct = 0;
+                int failct = 0;
+                int totalct = 0;
+                foreach (var item in RunTesting.Results.Keys)
+                {
+                    totalct++;
+                    if (RunTesting.Results[item] == true)
+                        passct++;
+                    else
+                        failct++;
+                }
+                log.Info("Total passes = " + passct);
+                log.Info("Total fails  = " + failct);
+                log.Info("Total Tests  = " + totalct);
+                log.Info("Asta la vista!");
             }
-            log.Info("Total passes = " + passct);
-            log.Info("Total fails  = " + failct);
-            log.Info("Total Tests  = " + totalct);
-            log.Info("Asta la vista!");
 
 
         }
